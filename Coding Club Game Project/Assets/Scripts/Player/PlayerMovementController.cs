21e6d307778df3c8a88943457c2cb9442e5ca831
Neole <nealdeng666@gmail.com>
2026-04-06 23:53:25 -0700

    Nothing lol i just messed with the input system and added a new input action asset, so this is just to commit those changes. I also changed the default volume profile and the street rule tile, but those are just small changes that I made while testing the input system, so they are not really important.

--- Coding Club Game Project/Assets/Scripts/Player/PlayerMovementController.cs
+++ Coding Club Game Project/Assets/Scripts/Player/PlayerMovementController.cs
@@ -1,7 +1,10 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovementController : MonoBehaviour
 {
+    public float PlayerSpeed = 5f;
+    public Vector2 movement;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
