f48a3c5a1cb73f1d43355e5d6eb86a0daa1235d1
Neole <nealdeng666@gmail.com>
2026-04-07 08:38:20 -0700

    Added player movement and jumping functionality. Adjusted player speed and jump force variables for better control. Though, the player doesn't stop moving until he hits a rigidbody wall.

--- Coding Club Game Project/Assets/Scripts/Player/PlayerMovementController.cs
+++ Coding Club Game Project/Assets/Scripts/Player/PlayerMovementController.cs
@@ -1,19 +1,49 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMovementController : MonoBehaviour
 {
-    public float PlayerSpeed = 5f;
-    public Vector2 movement;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public InputSystem_Actions actions;
+    public float PlayerSpeed;
+    public float JumpForce;
+    float movementX;
+    Rigidbody2D rb;
+    void Awake()
+    {
+        actions = new InputSystem_Actions();
+    }
+    void OnEnable()
+    {
+        actions.Player.Enable();
+        actions.Player.Move.performed += Movement;
+        actions.Player.Jump.performed += Jump;
+    }
+    void OnDisable()
+    {
+        actions.Player.Disable();
+        actions.Player.Move.performed -= Movement;
+        actions.Player.Jump.performed -= Jump;
+    }
+    void Movement(InputAction.CallbackContext context)
+    {
+        movementX = context.ReadValue<Vector2>().x;
+    }
+    void Jump(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            rb.linearVelocityY = JumpForce;
+        }
+    }
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rb.linearVelocityX = movementX * PlayerSpeed;
     }
 }
