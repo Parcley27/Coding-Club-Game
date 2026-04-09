8f7d3dc2d81456d0efc5b72051730d3491c45c2f
Kimi <Kjin@southridge.ca>
2026-04-09 16:10:05 -0700

    added jump height variation

--- Coding Club Game Project/Assets/Scripts/Player/PlayerMovementController.cs
+++ Coding Club Game Project/Assets/Scripts/Player/PlayerMovementController.cs
@@ -5,58 +5,108 @@ using UnityEngine.InputSystem;
 public class PlayerMovementController : MonoBehaviour
 {
     public InputSystem_Actions actions;
-    public float playerSpeed; //sets player speed multiplier
-    public float jumpForce; //determines how high jump is
+
+    [Header("Movement")]
+    public float playerSpeed = 5f;
+
+    [Header("Jump")]
+    public float jumpForce = 12f;          // upward speed applied while jumping
+    public float maxJumpHoldTime = 0.2f;   // how long player can hold jump for extra height
+
     float movementX;
     Rigidbody2D rb;
+
+    [Header("Ground Check")]
     public Transform groundedChecker;
-    public float groundCheckerRadius;
+    public float groundCheckerRadius = 0.2f;
     public LayerMask groundLayer;
     bool isGrounded;
+
+    bool isJumping;
+    float jumpTimeCounter;
+
     void Awake()
     {
         actions = new InputSystem_Actions();
     }
+
     void OnEnable()
     {
         actions.Player.Enable();
-        actions.Player.Move.performed += Movement;
-        actions.Player.Jump.performed += Jump;
 
+        actions.Player.Move.performed += Movement;
         actions.Player.Move.canceled += Movement;
-        actions.Player.Jump.canceled += Jump;
+
+        actions.Player.Jump.started += StartJump;
+        actions.Player.Jump.canceled += StopJump;
     }
+
     void OnDisable()
     {
-        actions.Player.Disable();
         actions.Player.Move.performed -= Movement;
-        actions.Player.Jump.performed -= Jump;
+        actions.Player.Move.canceled -= Movement;
+
+        actions.Player.Jump.started -= StartJump;
+        actions.Player.Jump.canceled -= StopJump;
+
+        actions.Player.Disable();
     }
-    void Movement(InputAction.CallbackContext context)
-    {
-        movementX = context.ReadValue<Vector2>().x;
-    }
-    void Jump(InputAction.CallbackContext context)
-    {
-        if (context.performed && isGrounded)
-        {
-            rb.linearVelocityY = jumpForce;
-        }
-    }
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+    void Movement(InputAction.CallbackContext context)
+    {
+        movementX = context.ReadValue<Vector2>().x;
+    }
+
+    void StartJump(InputAction.CallbackContext context)
+    {
+        if (isGrounded)
+        {
+            isJumping = true;
+            jumpTimeCounter = maxJumpHoldTime;
+            rb.linearVelocityY = jumpForce;
+        }
+    }
+
+    void StopJump(InputAction.CallbackContext context)
+    {
+        isJumping = false;
+    }
+
     void Update()
     {
-        isGrounded = Physics2D.OverlapCircle(groundedChecker.position, groundCheckerRadius, groundLayer);
+        isGrounded = Physics2D.OverlapCircle(
+            groundedChecker.position,
+            groundCheckerRadius,
+            groundLayer
+        );
+
         rb.linearVelocityX = movementX * playerSpeed;
+
+        if (isJumping && actions.Player.Jump.IsPressed())
+        {
+            if (jumpTimeCounter > 0f)
+            {
+                rb.linearVelocityY = jumpForce;
+                jumpTimeCounter -= Time.deltaTime;
+            }
+            else
+            {
+                isJumping = false;
+            }
+        }
     }
-    void OnDrawGizmosSelected() // This draws the radius of the ground checker in the Unity Scene and is not relevant for gameplay
+
+    void OnDrawGizmosSelected()
     {
-        Gizmos.color = Color.aliceBlue;
-        Gizmos.DrawWireSphere(groundedChecker.position, groundCheckerRadius);
+        Gizmos.color = Color.blue;
+        if (groundedChecker != null)
+        {
+            Gizmos.DrawWireSphere(groundedChecker.position, groundCheckerRadius);
+        }
     }
 }
